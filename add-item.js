4baf7d707953aae2b07e6600215d1d090d8c771b
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2023-05-11 11:25:36 +0200

    Fix

--- add-item.js
+++ add-item.js
@@ -1,107 +1,45 @@
 $(document).ready(() => {
-  const categorySelect = document.getElementById("categorySelect");
-  const sectionSelect = document.getElementById("sectionSelect");
-  const levelSelect = document.getElementById("levelSelect");
+  const locationInput = document.getElementById("locationInput");
   const barcodeInput = document.getElementById("barcodeInput");
+  const brandInput = document.getElementById("brandInput");
+  const modelInput = document.getElementById("modelInput");
+  const itemInputFields = document.getElementById("itemInputFields");
 
-  // Fyller inn hylleplasseringsvalgene basert på valgt kategori
-  categorySelect.addEventListener("change", () => {
-    const category = categorySelect.value;
-    const sections = Object.keys(inventory[category] || {});
-
-    // Tøm seksjons- og planvalgene
-    sectionSelect.innerHTML = "<option value=''>Velg seksjon</option>";
-    levelSelect.innerHTML = "<option value=''>Velg plan</option>";
-
-    // Fyll inn seksjonsvalgene
-    for (const section of sections) {
-      const option = document.createElement("option");
-      option.value = section;
-      option.textContent = section;
-      sectionSelect.appendChild(option);
-    }
+  locationInput.addEventListener("change", () => {
+    const location = locationInput.value;
+    itemInputFields.style.display = "block";
+    barcodeInput.focus();
   });
 
-  // Fyller inn planvalgene basert på valgt kategori og seksjon
-  sectionSelect.addEventListener("change", () => {
-    const category = categorySelect.value;
-    const section = sectionSelect.value;
-    const levels = Object.keys(inventory[category][section] || {});
-
-    // Tøm planvalgene
-    levelSelect.innerHTML = "<option value=''>Velg plan</option>";
-
-    // Fyll inn planvalgene
-    for (const level of levels) {
-      const option = document.createElement("option");
-      option.value = level;
-      option.textContent = level;
-      levelSelect.appendChild(option);
-    }
-  });
-
-  // Skanner strekkoden og fyller inn feltene automatisk
   barcodeInput.addEventListener("change", () => {
     const barcode = barcodeInput.value;
-    barcodeInput.value = barcode; // Kan endres til å tømme feltet etter skanning hvis ønskelig
-    // Bruk strekkoden for å fylle inn plassering og strekkodefelt automatisk
-    // Du kan bruke en relevant logikk for å hente plasseringen basert på strekkoden fra lageret
-    const category = categorySelect.value;
-    const section = sectionSelect.value;
-    const level = levelSelect.value;
-    const location = `H${category}.${section}.${level}`;
-
-    fillFieldsFromBarcode(barcode, location);
+    barcodeInput.value = barcode;
+    brandInput.focus();
   });
 
-  // Funksjon for å fylle inn feltene basert på strekkode og plassering
-  const fillFieldsFromBarcode = (barcode, location) => {
-    const item = inventory[location] && inventory[location][barcode];
-
-    if (item) {
-      categorySelect.value = item.category;
-      sectionSelect.value = item.section;
-      levelSelect.value = item.level;
-      barcodeInput.value = barcode;
-    }
-  };
-
-  // Håndterer innsending av skjemaet for å legge til varen
   const addItemForm = document.getElementById("addItemForm");
   addItemForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const category = categorySelect.value;
-    const section = sectionSelect.value;
-    const level = levelSelect.value;
-    const location = `H${category}.${section}.${level}`;
-    const brand = addItemForm.elements.brand.value;
-    const model = addItemForm.elements.model.value;
+    const location = locationInput.value;
     const barcode = barcodeInput.value;
+    const brand = brandInput.value;
+    const model = modelInput.value;
 
-    // Opprett en ny vare basert på innsendt data
     const newItem = {
-      category,
-      section,
-      level,
       brand,
       model,
       barcode,
+      location
     };
 
-    inventory[category] = inventory[category] || {};
-    inventory[category][section] = inventory[category][section] || {};
-    inventory[category][section][level] = inventory[category][section][level] || {};
-    inventory[category][section][level][location] = newItem;
+    inventory[location] = newItem;
 
     res.status(201).json({ message: "Vare lagt til", location, item: newItem });
 
     saveInventoryToFile(inventory);
 
-    // Tilbakestill skjemaet
     addItemForm.reset();
-    categorySelect.value = "";
-    sectionSelect.innerHTML = "<option value=''>Velg seksjon</option>";
-    levelSelect.innerHTML = "<option value=''>Velg plan</option>";
+    itemInputFields.style.display = "none";
   });
 });
