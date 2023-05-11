913caf524aaf049f03dc3b1b7b56f0bdadf3e304
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2023-05-11 09:46:17 +0200

    Fix

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 const fs = require("fs");
 
 app.use(express.static("public"));
