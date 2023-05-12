43e6a6ffe85d019d831ba9cf5dcc87eee0d9349e
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2023-05-12 11:27:26 +0200

    Update delete-item.js

--- public/delete-item.js
+++ public/delete-item.js
@@ -20,15 +20,15 @@ $(document).ready(() => {
   const addItemForm = document.getElementById("addItemForm");
 
   function showAlert(title, message, type) {
-    const alertElement = $(`
-      <div class="alert alert-${type}" role="alert">
-      <strong>${title}</strong> ${message}
-      </div>
-      `);
-    $(".alerts-container").append(alertElement);
-    setTimeout(() => {
-      alertElement.remove();
-    }, 3000);
+    Swal.fire({
+      title: title,
+      text: message,
+      icon: type,
+      timer: 3000,
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+    });
   }
 
   function fetchInventory() {
@@ -58,32 +58,36 @@ $(document).ready(() => {
 
     // Hent lageret fra serveren
     fetch("/inventory")
-    .then((response) => response.json())
-    .then((data) => {
-      const items = Object.entries(data).flatMap(([category, sections]) =>
-        Object.entries(sections).flatMap(([section, levels]) =>
-          Object.entries(levels).flatMap(([level, items]) =>
-            Object.entries(items).map(([location, item]) => {
-              return {
-                item,
-                location: `H21.${category}.${section}.${level}`,
-              };
-            })
+      .then((response) => response.json())
+      .then((data) => {
+        const items = Object.entries(data).flatMap(([category, sections]) =>
+          Object.entries(sections).flatMap(([section, levels]) =>
+            Object.entries(levels).flatMap(([level, items]) =>
+              Object.entries(items).map(([location, item]) => {
+                return {
+                  item,
+                  location: `H21.${category}.${section}.${level}`,
+                };
+              })
             )
           )
         );
 
-      inventoryTableBody.empty();
+        inventoryTableBody.empty();
 
-      items.forEach(({ item, location }) => {
-        const row = createTableRow(item, location);
-        inventoryTableBody.append(row);
+        items.forEach(({ item, location }) => {
+          const row = createTableRow(item, location);
+          inventoryTableBody.append(row);
+        });
+      })
+      .catch((error) => {
+        console.error("Feil ved henting av lageret:", error);
+        showAlert(
+          "Feil",
+          "Noe gikk galt ved henting av lageret. Vennligst prøv igjen.",
+          "error"
+        );
       });
-    })
-    .catch((error) => {
-      console.error("Feil ved henting av lageret:", error);
-      showAlert("Feil", "Noe gikk galt ved henting av lageret. Vennligst prøv igjen.", "error");
-    });
   }
 
   // Oppdater tabellen ved sidenlast
@@ -94,23 +98,28 @@ $(document).ready(() => {
     fetch(`/inventory/${barcode}`, {
       method: "DELETE",
     })
-    .then((response) => {
-      if (response.ok) {
-        showAlert("Suksess", "Vare slettet!", "success");
-        const row = $(`tr:has(td:contains(${barcode}))`);
-        row.remove();
-      } else {
-        console.error("Feil ved sletting av vare:", response);
-        showAlert("Feil", "Noe gikk galt under sletting av vare. Vennligst prøv igjen.", "error");
-      }
-    })
-    .catch((error) => {
-      console.error("Feil ved sletting av vare:", error);
-      showAlert("Feil", "Noe gikk galt under sletting av vare. Vennligst prøv igjen.", "error");
-    });
+      .then((response) => {
+        if (response.ok) {
+          showAlert("Suksess", "Vare slettet!", "success");
+          const row = $(`tr:has(td:contains(${barcode}))`);
+          row.remove();
+        } else {
+          console.error("Feil ved sletting av vare:", response);
+        showAlert("Feil", "Noe gikk galt under sletting av vare. Vennligst prøv igjen.",
+            "error");
+        }
+      })
+      .catch((error) => {
+        console.error("Feil ved sletting av vare:", error);
+        showAlert(
+          "Feil",
+          "Noe gikk galt under sletting av vare. Vennligst prøv igjen.",
+          "error"
+        );
+      });
   }
 
-// Legg til vare
+  // Legg til vare
   addItemForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const location = locationInput.value;
@@ -132,20 +141,28 @@ $(document).ready(() => {
       },
       body: JSON.stringify(newItem),
     })
-    .then((response) => {
-      if (response.ok) {
-        showAlert("Suksess", "Vare lagt til!", "success");
-        addItemForm.reset();
-        itemInputFields.style.display = "none";
-      fetchInventory(); // Oppdater tabellen
-    } else {
-      console.error("Feil ved lagring av vare:", response);
-      showAlert("Feil", "Noe gikk galt ved lagring av vare. Vennligst prøv igjen.", "error");
-    }
-  })
-    .catch((error) => {
-      console.error("Feil ved lagring av vare:", error);
-      showAlert("Feil", "Noe gikk galt ved lagring av vare. Vennligst prøv igjen.", "error");
-    });
+      .then((response) => {
+        if (response.ok) {
+          showAlert("Suksess", "Vare lagt til!", "success");
+          addItemForm.reset();
+          itemInputFields.style.display = "none";
+          fetchInventory(); // Oppdater tabellen
+        } else {
+          console.error("Feil ved lagring av vare:", response);
+          showAlert(
+            "Feil",
+            "Noe gikk galt ved lagring av vare. Vennligst prøv igjen.",
+            "error"
+          );
+        }
+      })
+      .catch((error) => {
+        console.error("Feil ved lagring av vare:", error);
+        showAlert(
+          "Feil",
+          "Noe gikk galt ved lagring av vare. Vennligst prøv igjen.",
+          "error"
+        );
+      });
   });
 });
