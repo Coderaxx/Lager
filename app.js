35994c41527ed14893ab74f8ba5232c16072cfe4
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2023-05-18 20:45:34 +0200

    Update

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ const path = require("path");
 const fs = require("fs");
 const Sentry = require("@sentry/node");
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://coderax:dJp4F1u9@kVgpzJ^E9Gr@cluster0.xlok50g.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 Sentry.init({
   dsn: "https://3c602fc817b942cbaf82608c5d9450fe@o4505183347081216.ingest.sentry.io/4505183347081216",
 
