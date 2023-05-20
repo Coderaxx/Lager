d01a673c8ee215baf2efe5630705860b2e13bcee
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2023-05-20 06:56:07 +0200

    Fix

--- public/add-item.js
+++ public/add-item.js
@@ -64,8 +64,21 @@ $(document).ready(async () => {
   async function searchOnninenByBarcode(barcode) {
     const config = {
       headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "nb-NO,nb;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Host": "www.onninen.no",
+        "Origin": "https://www.onninen.no",
+        "Pragma": "no-cache",
+        "Referer": "https://www.onninen.no/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same - origin",
+        "User-Agent": "Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+
       },
     };
     axios.get(`https://www.onninen.no/rest/v2/search/suggestion?term=${barcode}`, config)
