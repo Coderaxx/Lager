1742456da5549401419f65c82bd3575cfde41fe4
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2023-05-26 06:19:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ function searchInventory(query) {
     return 1;
   };
 
-  for (const category of inventory[0].shelves) {
+  for (const category of inventory.shelves) {
     for (const shelf of category.levels) {
       const items = shelf.items;
       const itemLocation = `H21.${category.name}${shelf.name}`;
